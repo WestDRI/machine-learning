9ea2962375edc8655f4d7fcdf044ad39835ee3f1
Marie-Helene Burle <marie.burle@westgrid.ca>
2020-06-25 15:32:06 -0700

    add first draft script mnist.py for school remake

---
+++ static/src/schoolremake/mnist.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import datasets, transforms
+
+# To run on GPU, replace 'cpu' by 'cuda'
+device = torch.device('cpu')
+
+train = datasets.MNIST(
+    './data',
+    train = True,
+    download = True,
+    transform = transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))]))
+
+test = datasets.MNIST(
+    './data',
+    train = False,
+    download = False,
+    transform = transforms.Compose([
+                       transforms.ToTensor(),
+                       transforms.Normalize((0.1307,), (0.3081,)) ]))
+
+train_loader = torch.utils.data.DataLoader(
+    train,
+    batch_size = 4,
+    shuffle = True)
+
+test_loader = torch.utils.data.DataLoader(
+    test,
+    batch_size = 4,
+    shuffle = False)
